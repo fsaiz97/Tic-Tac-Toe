cf6cab2705b4af82a16fa4e584b1846a27a6f90d
Felix Saiz <felixsaiz97@gmail.com>
2022-04-20 15:34:07 +0100

    Remade basic game with no classes, need to add AI player and fix stderr printing in between stdout outputs

--- tictactoe.py
+++ tictactoe.py
@@ -1,188 +1,83 @@
 import sys
+import numpy as np
 
 
-def get_possibles(n):
-    # get rows
-    possible = [[(x, y) for x in range(n)] for y in range(n)]
-    # get columns
-    possible += [[(x, y) for y in range(n)] for x in range(n)]
-    # get diagonals
-    possible += [[(i, i) for i in range(n)], [(i, n-1-i) for i in range(n)]]
-
-    return possible
-
-
-def check_win(board, possible):
-    for line in possible:
-        symbols = set(board[coord[1]][coord[0]] for coord in line)
-        if len(symbols) != 1:
-            continue
+def check_line(line):
+    if np.all(np.isclose(line, line[0])):
+        if line[0] == 0:
+            return False
+        elif line[0] in (1,2):
+            return True
         else:
-            symbol = next(iter(symbols))
-        if symbol == None:
-            continue
-        else:
-            return symbol
-    return False
+            raise ValueError("Impossible symbol on board")
 
 
-class Player:
-    def __init__(self, name, ai=False):
-        self.name = name
-        self.isAI = ai
-        self.records = {"wins": 0, "losses": 0, "draws": 0}
+def is_win(game_board, BOARD_SIZE):
+    for i in range(BOARD_SIZE):
+        if check_line(game_board[:,i]) or check_line(game_board[i,:]):
+            return True
 
-    def ai_pick(self):
-        import random
+    if check_line(game_board.diagonal()) or check_line(np.fliplr(game_board).diagonal()):
+        return True
 
-        return [random.randint(0,2) for _ in range(2)]
-
-    def get_input(self):
-        if not active.isAI:
-            while True:
-                try:
-                    x, y = tuple(map(int,
-                        input(f'({active.name}) Enter two integers between 0 and {self.n-1}:').split()))
-                    break
-                except ValueError:
-                    print("Error: Input must be integers." , file=sys.stderr)
-                    continue
-        else:
-            x, y = active.ai_pick()
+    return False  # if no full lines are found
 
 
-class Game:
-    def __init__(self, n, player1, player2):
-        self.board = tuple([None for _ in range(n)] for _ in range(n))
-        self.size = n
-        player1.symbol, player2.symbol = 'O', 'X'
-        self.players = (player1, player2)
-        self.turn = 0
-        self.state = "ongoing"
+def get_move(BOARD_SIZE):
+    try:
+        coordinate = tuple(map(int, input(f'Enter two integers between 1 and {BOARD_SIZE}: ').split()))
+    except ValueError as err:
+        raise ValueError("Integer inputs expected") from err
 
+    for component in coordinate:
+        if not (1 <= component <= BOARD_SIZE):
+            raise ValueError("Input is outside the expected range")
 
-    def place_selection(self):
-        try:
-            self.board[y][x] = active.symbol
-        except IndexError:
-            print(f"Error: Input should be between 0 and {self.n-1}.", file=sys.stderr)
-            continue
-
-
-    def print_board(self):
-        for line in self.board:
-            print(line)
-
-
-    def run_turn(self):
-        # select active player's name based on turn
-        active = self.players[self.turn % 2]
-
-        x, y = active.get_input()
-
-        place_selection()
-
-        print_board()
-
-        result = check_win(self.board, possible)
-        if result != False:
-            print(f"Winner is player {result}")
-            break
-
-        self.turn += 1
-
-
-class Player:
-    def __init__(self, name, ai=False):
-        self.name = name
-        self.isAI = ai
-        self.records = {"wins": 0, "losses": 0, "draws": 0}
-
-    def ai_pick(self):
-        import random
-
-        return [random.randint(0,2) for _ in range(2)]
-
-
-class Game:
-    def __init__(self, n, player1, player2):
-        self.board = tuple([None for _ in range(n)] for _ in range(n))
-        self.size = n
-        player1.symbol, player2.symbol = 'O', 'X'
-        self.players = (player1, player2)
-        self.turn = 0
-        self.state = "ongoing"
-
-
-    def run_turn(self):
-        # select active player's name based on turn
-        active = self.players[self.turn % 2]
-
-        if not active.isAI:
-            while True:
-                try:
-                    x, y = tuple(map(int,
-                                     input(f'({active.name}) Enter two integers between 0 and {n-1}:').split()))
-                    break
-                except ValueError:
-                    print("Error: Input must be integers." , file=sys.stderr)
-                    continue
-        else:
-            x, y = active.ai_pick()
-
-        try:
-            self.board[y][x] = active.symbol
-        except IndexError:
-            print(f"Error: Input should be between 0 and {n-1}.", file=sys.stderr)
-            continue
-
-        for line in self.board:
-            print(line)
-
-        result = check_win(self.board, possible)
-        if result != False:
-            print(f"Winner is player {result}")
-            break
-
-        self.turn += 1
+    return (coordinate[1]-1, coordinate[0]-1)  # converts from human convenient coordinates to flipped index 0
+    # coordinates
 
 
 def main():
-    n = 3  # board size
-    human = Player(input("name = "))
-    comp = Player("comp", ai=True)
-    game = Game(3, human, comp)
-    possible = get_possibles(game.size)
+    BOARD_SIZE = 3
+    game_board = np.zeros((BOARD_SIZE, BOARD_SIZE), dtype=int)
+    print("Current State:\n", game_board)
+
+    player_current = 1
 
     while True:
-        # select active player's name based on turn
-        active = game.players[game.turn % 2]
-
-        if not active.isAI:
+        while True:
             try:
-                x, y = tuple(map(int,
-                                 input(f'({active.name}) Enter two integers between 0 and {n-1}:').split()))
-            except ValueError:
-                print("Error: Input must be integers." , file=sys.stderr)
-                continue
-        else:
-            x, y = active.ai_pick()
+                command = input(f' Enter m to make a move or enter q to quit: ')
+                if command == 'm':
+                    while True:
+                        try:
+                            move = get_move(BOARD_SIZE)
+                            if game_board[move[0]][move[1]] != 0:
+                                raise ValueError("Board position already filled")
+                        except ValueError as err:
+                            print(err, file=sys.stderr)
+                        else:
+                            break
+                elif command == 'q':
+                    exit("Quitting game...")
+                else:
+                    raise ValueError("Invalid command")
+            except ValueError as err:
+                print(err, file=sys.stderr)
+            else:
+                break
 
-        try:
-            game.board[y][x] = active.symbol
-        except IndexError:
-            print(f"Error: Input should be between 0 and {n-1}.", file=sys.stderr)
-            continue
 
-        for line in game.board:
-            print(line)
+        game_board[move[0]][move[1]] = player_current
 
-        result = check_win(game.board, possible)
-        if result != False:
-            print(f"Winner is player {result}")
+        print(game_board)
+
+        result = is_win(game_board, BOARD_SIZE)
+        if result:
+            print(f"Winner is player {player_current}")
             break
 
-        game.turn += 1
+        player_current = 2 if player_current == 1 else 1
 
 
 if __name__ == '__main__':
