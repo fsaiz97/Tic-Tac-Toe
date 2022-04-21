0ec17f346c4b5638daa2fea3c2407ff08f709005
fsaiz97 <75044237+fsaiz97@users.noreply.github.com>
2022-04-21 10:55:18 +0100

    Added comments as necessary Added basic robot player

--- tictactoe.py
+++ tictactoe.py
@@ -3,55 +3,56 @@ import numpy as np
 
 
 def check_line(line):
-    if np.all(np.isclose(line, line[0])):
-        if line[0] == 0:
+    if np.all(np.isclose(line, line[0])):  # checks if all of the squares in the line are equal to each other
+        if line[0] == 0:  # since 0 represents an empty square
             return False
-        elif line[0] in (1,2):
+        elif line[0] in (1, 2):
             return True
         else:
             raise ValueError("Impossible symbol on board")
 
 
-def is_win(game_board, BOARD_SIZE):
-    for i in range(BOARD_SIZE):
-        if check_line(game_board[:,i]) or check_line(game_board[i,:]):
+def is_win(game_board, board_size):
+    for i in range(board_size):
+        if check_line(game_board[:, i]) or check_line(game_board[i, :]):  # checks for completed row or column
             return True
 
-    if check_line(game_board.diagonal()) or check_line(np.fliplr(game_board).diagonal()):
+    if check_line(game_board.diagonal()) or check_line(np.fliplr(game_board).diagonal()):  # checks for completed
+        # diagonal or anti-diagonal
         return True
 
     return False  # if no full lines are found
 
 
-def get_move(BOARD_SIZE):
+def get_move(board_size):
     try:
-        coordinate = tuple(map(int, input(f'Enter two integers between 1 and {BOARD_SIZE}: ').split()))
+        coordinate = tuple(map(int, input(f'Enter two integers between 1 and {board_size}: ').split()))
     except ValueError as err:
         raise ValueError("Integer inputs expected") from err
 
     for component in coordinate:
-        if not (1 <= component <= BOARD_SIZE):
+        if not (1 <= component <= board_size):
             raise ValueError("Input is outside the expected range")
 
-    return (coordinate[1]-1, coordinate[0]-1)  # converts from human convenient coordinates to flipped index 0
+    return coordinate[1] - 1, coordinate[0] - 1  # converts from human convenient coordinates to flipped index 0
     # coordinates
 
 
 def main():
-    BOARD_SIZE = 3
-    game_board = np.zeros((BOARD_SIZE, BOARD_SIZE), dtype=int)
+    board_size = 3
+    game_board = np.zeros((board_size, board_size), dtype=int)
     print("Current State:\n", game_board)
 
     player_current = 1
 
-    while True:
-        while True:
+    while True:  # game loop
+        while True:  # command loop
             try:
                 command = input(f' Enter m to make a move or enter q to quit: ')
                 if command == 'm':
-                    while True:
+                    while True:  # move input loop
                         try:
-                            move = get_move(BOARD_SIZE)
+                            move = get_move(board_size)
                             if game_board[move[0]][move[1]] != 0:
                                 raise ValueError("Board position already filled")
                         except ValueError as err:
@@ -67,12 +68,15 @@ def main():
             else:
                 break
 
+        try:
+            game_board[move[0]][move[1]] = player_current
+        except NameError as err:  # Throws error if variable move is not set
+            print(err)
+            exit(-1)
 
-        game_board[move[0]][move[1]] = player_current
+        print("Current State:\n", game_board)
 
-        print(game_board)
-
-        result = is_win(game_board, BOARD_SIZE)
+        result = is_win(game_board, board_size)
         if result:
             print(f"Winner is player {player_current}")
             break
