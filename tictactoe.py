4ee73c07fb7faec16cdd11bb7bdfd8666b56b22e
Felix Saiz <felixsaiz97@gmail.com>
2022-04-22 17:39:52 +0100

    Added class for the game board to store the game state, also prints the board and handles placing a mark/piece

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,41 @@
-import sys
 import numpy as np
 import random
+
+
+class Board:
+    """A class representing a game board"""
+
+    def __init__(self, width, height=0):
+        self.width = width
+        if height != 0:
+            self.height = height
+        else:
+            self.height = width
+
+        self.state = np.zeros((self.height, self.width), dtype=int)
+
+    def __str__(self):
+        return '\n'.join(['|'.join([str(mark) for mark in row]) for row in self.state])
+
+    def is_occupied(self, coordinate):
+        if self.state[coordinate[0]][coordinate[1]] == 0:
+            return False
+        else:
+            return True
+
+    def place_mark(self, move, active_mark):
+        # catches out of range input
+        try:
+            if not self.is_occupied(move):
+                self.state[move[0]][move[1]] = active_mark
+            else:
+                raise ValueError("Position is occupied")
+        except ValueError as err:
+            print(err)
+            raise
+        except IndexError:
+            print("IndexError: Move is out of bounds")
+            raise
 
 
 def check_line(line):
@@ -13,27 +48,29 @@ def check_line(line):
             raise ValueError("Impossible symbol on board")
 
 
-def is_win(game_board, board_size):
-    for i in range(board_size):
-        if check_line(game_board[:, i]) or check_line(game_board[i, :]):  # checks for completed row or column
+def is_win(game_board):
+    for i in range(game_board.width):
+        if check_line(game_board.state[:, i]):  # checks for completed columns
             return True
 
-    if check_line(game_board.diagonal()) or check_line(np.fliplr(game_board).diagonal()):  # checks for completed
-        # diagonal or anti-diagonal
+    for j in range(game_board.height):
+        if check_line(game_board.state[j, :]):  # checks for completed rows
+            return True
+
+    # need to update this to check for all diagonals with correct length or only allow square boards?
+    if check_line(game_board.state.diagonal()) or check_line(np.fliplr(game_board.state).diagonal()):
+        # checks for completed diagonal or anti-diagonal
         return True
 
     return False  # if no full lines are found
 
 
-def get_move(board_size):
+def get_move():
     try:
-        coordinate = tuple(map(int, input(f'Enter two integers between 1 and {board_size}: ').split()))
+        coordinate = tuple(map(int, input(f'Enter two integers within the width and height bounds '
+                                          f'respectively: ').split()))
     except ValueError as err:
         raise ValueError("Integer inputs expected") from err
-
-    for component in coordinate:
-        if not (1 <= component <= board_size):
-            raise ValueError("Input is outside the expected range")
 
     return coordinate[1] - 1, coordinate[0] - 1  # converts from human convenient coordinates to flipped index 0
     # coordinates
@@ -41,8 +78,8 @@ def get_move(board_size):
 
 def main():
     board_size = 3
-    game_board = np.zeros((board_size, board_size), dtype=int)
-    print("Current State:\n", game_board)
+    game_board = Board(board_size)
+    print("Current State:\n", game_board, sep="")
 
     player_current = 1
 
@@ -55,18 +92,16 @@ def main():
                     if command not in ['m', 'q']:
                         raise ValueError("Invalid command")
                 except ValueError as err:
-                    print(err, file=sys.stderr)
+                    print(err)
                 else:
                     break
 
             if command == 'm':
                 while True:  # move input loop
                     try:
-                        move = get_move(board_size)
-                        if game_board[move[0]][move[1]] != 0:
-                            raise ValueError("Board position already filled")
+                        move = get_move()
                     except ValueError as err:
-                        print(err, file=sys.stderr)
+                        print(err)
                     else:
                         break
             elif command == 'q':
@@ -75,19 +110,21 @@ def main():
         else:
             # code for processing AI player input
             while True:
-                move = (random.randrange(board_size), random.randrange(board_size))
-                if game_board[move[0]][move[1]] == 0:
+                move = (random.randrange(game_board.height), random.randrange(game_board.width))
+                if not game_board.is_occupied(move):
                     break
 
         try:  # Places mark at chosen position
-            game_board[move[0]][move[1]] = player_current
+            game_board.place_mark(move, player_current)
         except NameError as err:  # Throws error if variable move is not set
             print(err)
             exit(-1)
+        except (IndexError, ValueError):
+            continue
 
-        print("Current State:\n", game_board)
+        print("Current State:\n", game_board, sep="")
 
-        result = is_win(game_board, board_size)
+        result = is_win(game_board)
         if result:
             print(f"Winner is player {player_current}")
             break
