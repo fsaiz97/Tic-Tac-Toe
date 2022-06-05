516ba8f1992bfadbb75c1aea353a5f1f370fb840
Felix Saiz <felixsaiz97@gmail.com>
2022-06-05 16:26:14 +0100

    Intermediate: Added more pygame functionality. To-do: - Re-organise game logic among main loop and classes. - Add proper docstrings - Start adding Pygame graphics

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,15 @@
 import itertools
 
 import pygame
+from pygame.locals import (
+    MOUSEBUTTONDOWN,
+    KEYDOWN,
+    K_ESCAPE,
+    QUIT,
+)
 
-from classes import *
-from functions import *
+from game_classes import *
+from game_functions import *
 
 Coordinate = Tuple[int, int]
 
@@ -12,24 +18,53 @@ def main() -> None:
     """Placeholder docstring"""
     board_size: int = 3
     game_board: Board = Board(board_size)
+    # screen variables for pygame
+    SCREEN_WIDTH = 600
+    SCREEN_HEIGHT = 600
+
+    # colour variables for pygame
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
 
     player_list = [PlayerHuman("Human", 1), PlayerAI("AI", 2)]
     random.shuffle(player_list)
     player_list = itertools.cycle(player_list)
 
+    # pygame initialization starts
     pygame.init()
 
-    # screen variables for pygame
-    SCREEN_WIDTH = 800
-    SCREEN_HEIGHT = 600
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill(WHITE)
 
-    print("Current State:\n", game_board, sep="")
+    for x in range(0, SCREEN_HEIGHT, SCREEN_HEIGHT//3):
+        for y in range(0, SCREEN_WIDTH, SCREEN_WIDTH//3):
+            rect = pygame.Rect(x, y, SCREEN_HEIGHT//3, SCREEN_WIDTH//3)
+            pygame.draw.rect(screen, BLACK, rect, 1)
 
-    player_current: Player = next(player_list)
+    pygame.display.flip()
+    # pygame initialization ends
 
-    running = True
+    running = False
+    runningPygame = True
+
+    # pygame loop
+    while runningPygame:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    runningPygame = False
+            elif event.type == QUIT:
+                runningPygame = False
+            elif event.type == MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                rect = pygame.Rect(*mouse_pos, 20, 20)
+                pygame.draw.rect(screen, BLACK, rect, 1)
+
+            pygame.display.flip()
+
+
+    #player_current: Player = next(player_list)
+    # print("Current State:\n", game_board, sep="")
 
     # game loop
     while running:
