09cc6620aef81cc3a455584e770b6fa03136a17c
fsaiz97 <75044237+fsaiz97@users.noreply.github.com>
2022-06-06 21:34:54 +0100

    Refactored names to reveal intention behind variables. To-do: - Re-organise game logic among main loop and classes. - Add proper docstrings - Start adding Pygame graphics

--- pygame_classes.py
+++ pygame_classes.py
@@ -8,9 +8,9 @@ MouseClickStates = Tuple[bool, bool, bool]  # type for the tuple returned by pyg
 class Button(pygame.sprite.Sprite):
     """Basic button class that activates when clicked"""
 
-    def __init__(self, width, height, colour=pygame.Color("White")):
+    def __init__(self, pixel_width, pixel_height, colour=pygame.Color("White")):
         super(Button, self).__init__()
-        self.image = pygame.Surface((width, height))
+        self.image = pygame.Surface((pixel_width, pixel_height))
         self.image.fill(colour)
         self.rect = self.image.get_rect()
 
@@ -24,16 +24,20 @@ class Button(pygame.sprite.Sprite):
 class Grid(pygame.sprite.Sprite):
     """Collects Button instances into a grid game-board"""
 
-    def __init__(self, n_rows, n_columns, width, height):
+    def __init__(self, number_of_rows, number_of_columns, pixel_height, pixel_width):
         super(Grid, self).__init__()
-        self.width = width
-        self.height = height
-        self.array = [[Button(width/n_columns, height/n_rows, pygame.Color("Black"))] for row in range(0, n_rows)]
-        self.image = pygame.Surface((width, height))
+        self.pixel_width = pixel_width
+        self.pixel_height = pixel_height
+        button_pixel_width = self.pixel_width // number_of_columns
+        button_pixel_height = self.pixel_height // number_of_rows
+        self.button_array = [[Button(button_pixel_width, button_pixel_height, pygame.Color("Red"))
+                              for j in range(0, number_of_columns)] for i in range(0, number_of_rows)]
+        self.image = pygame.Surface((self.pixel_width, pixel_height))
         self.image.fill(pygame.Color("White"))
-        for i, x in enumerate(range(0, self.height, self.height // n_rows)):
-            for j, y in enumerate(range(0, self.width, self.width // n_columns)):
-                self.array[i][j].draw(self.image, (x, y))
+
+        for i, x in enumerate(range(0, self.pixel_height, button_pixel_height)):
+            for j, y in enumerate(range(0, self.pixel_width, button_pixel_width)):
+                self.button_array[i][j].draw(self.image, (x, y))
         self.rect = self.image.get_rect()
 
     def draw(self, screen, pos):
--- pygame_functions.py
+++ pygame_functions.py
@@ -1,9 +1,8 @@
 import pygame
 
 
-def draw_grid(screen, SCREEN_HEIGHT, SCREEN_WIDTH, colour):
-    for x in range(0, SCREEN_HEIGHT, SCREEN_HEIGHT // 3):
-        for y in range(0, SCREEN_WIDTH, SCREEN_WIDTH // 3):
-            rect = pygame.Rect(x, y, SCREEN_HEIGHT // 3, SCREEN_WIDTH // 3)
+def draw_grid(screen, screen_height, screen_width, colour):
+    for x in range(0, screen_height, screen_height // 3):
+        for y in range(0, screen_width, screen_width // 3):
+            rect = pygame.Rect(x, y, screen_height // 3, screen_width // 3)
             pygame.draw.rect(screen, colour, rect, 1)
-
