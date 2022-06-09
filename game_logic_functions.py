46b06d3f30495548efa9e28b3d0632b682cd5440
fsaiz97 <75044237+fsaiz97@users.noreply.github.com>
2022-06-09 23:42:45 +0100

    Intermediate: Refactoring according to Clean Code. Working on functions. To-do: - Re-organise game logic among main loop and classes. - Add proper docstrings - Start adding Pygame graphics

--- game_logic_functions.py
+++ game_logic_functions.py
@@ -5,26 +5,30 @@ from game_logic_classes import Board
 from game_logic_constants import Tile
 
 
-def check_line(line: numpy.typing.ArrayLike) -> bool:
+def is_line_empty(line_element):
+    if line_element == Tile.EMPTY:  # since 0 represents an empty square
+        return False
+    elif line_element in (Tile.PLAYER_1, Tile.PLAYER_2):
+        return True
+    else:
+        raise ValueError("Impossible symbol on board")
+
+
+def check_line_for_win(line: numpy.typing.ArrayLike) -> bool:
     """Placeholder docstring"""
     if numpy.all(numpy.equal(line, line[0])):  # checks if all the marks in the line are equal
-        if line[0] == Tile.EMPTY:  # since 0 represents an empty square
-            return False
-        elif line[0] in (Tile.PLAYER_1, Tile.PLAYER_2):
-            return True
-        else:
-            raise ValueError("Impossible symbol on board")
+        return is_line_empty(line[0])
 
 
 def is_win(game_board: Board) -> bool:
     """Placeholder docstring"""
     # checks for completed rows or columns
     for i in range(game_board.size):
-        if check_line(game_board.state[:, i]) or check_line(game_board.state[i, :]):
+        if check_line_for_win(game_board.state[:, i]) or check_line_for_win(game_board.state[i, :]):
             return True
 
     # checks for completed diagonal or anti-diagonal
-    if check_line(game_board.state.diagonal()) or check_line(numpy.fliplr(game_board.state).diagonal()):
+    if check_line_for_win(game_board.state.diagonal()) or check_line_for_win(numpy.fliplr(game_board.state).diagonal()):
         return True
 
     return False  # if no complete lines are found
