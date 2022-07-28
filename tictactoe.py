34ec608ec223a43578af484a188ed00398a87d39
Felix Saiz <felixsaiz97@gmail.com>
2022-07-28 08:38:52 +0100

    - Added option to choose between command line and graphics versions of the game. To-do: - Refactor until adding new features is easy. - Add proper docstrings - Make the graphical game play as expected.

--- tictactoe.py
+++ tictactoe.py
@@ -13,63 +13,75 @@ Coordinate = Tuple[int, int]
 def main() -> None:
     """Placeholder docstring"""
 
-    # initialize game, display and players
+    # initialize game and players
     pygame.init()
     game = initialize_game()
-    game_window = initialize_game_window(game.get_number_of_partitions())
-    tile_set = load_tiles()
     # start game
     # event loop: react to mouse clicks, exit on clicking exit button or closing window
     # clean up game
 
-    pygame.display.flip()
-
     # choose between cli game loop and pygame game loop
-    running = False
-    running_pygame = True
 
-    # pygame loop
-    while running_pygame:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                running_pygame = False
-            elif event.type == pygame.QUIT:
-                running_pygame = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_position = pygame.mouse.get_pos()
-                grid_pos = get_grid_pos(game_window.cell_width, game_window.cell_height, mouse_position)
-                cell_start_corner = grid_pos[0]*game_window.cell_width, grid_pos[1]*game_window.cell_height
-                tile = tile_set.tiles[1]
-                cell_size = (game_window.cell_width, game_window.cell_height)
-                tile = pygame.transform.scale(tile, cell_size)
-                game_window.display_surface.blit(tile, cell_start_corner)
+    while True:
+        graphics_choice = input("Do you want graphics? (Y/y/N/n)\n").lower()
+        if graphics_choice not in ['y', 'n']:
+            print("Invalid choice, Please try again.\n")
+        elif graphics_choice == 'y':
+            graphics_on = True
+            break
+        else:
+            graphics_on = False
+            break
 
-            pygame.display.flip()
+    running = True
 
-    # game loop
-    while running:
-        player_move = game.player_current.get_move()
+    if graphics_on:
+        # initialize graphics
+        game_window = initialize_game_window(game.get_number_of_partitions())
+        tile_set = load_tiles()
 
-        try:
-            game.board.place_symbol(player_move, game.player_current.symbol)
-        # Throws error if variable move is not set
-        except NameError as error:
-            print(error)
-            exit(-1)
-        except (IndexError, ValueError) as error:
-            print(error)
-            continue
+        # pygame loop
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    running = False
+                elif event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_position = pygame.mouse.get_pos()
+                    grid_pos = get_grid_pos(game_window.cell_width, game_window.cell_height, mouse_position)
+                    cell_start_corner = grid_pos[0]*game_window.cell_width, grid_pos[1]*game_window.cell_height
+                    tile = tile_set.tiles[1]
+                    cell_size = (game_window.cell_width, game_window.cell_height)
+                    tile = pygame.transform.scale(tile, cell_size)
+                    game_window.display_surface.blit(tile, cell_start_corner)
 
-        print("Current State:\n", game.board, sep="")
+                pygame.display.flip()
+    else:
+        # game loop
+        while running:
+            player_move = game.player_current.get_move()
 
-        game_result: bool = is_win(game.board)
-        if game_result:
-            print(f"Winner is player {game.player_current.name}")
-            running = False
+            try:
+                game.board.place_symbol(player_move, game.player_current.symbol)
+            # Throws error if variable move is not set
+            except NameError as error:
+                print(error)
+                exit(-1)
+            except (IndexError, ValueError) as error:
+                print(error)
+                continue
 
-        if running:
-            print("turn end")
-            game.player_current = game.get_next_player()
+            print("Current State:\n", game.board, sep="")
+
+            game_result: bool = is_win(game.board)
+            if game_result:
+                print(f"Winner is player {game.player_current.name}")
+                running = False
+
+            if running:
+                print("turn end")
+                game.player_current = game.get_next_player()
 
 
 if __name__ == '__main__':
